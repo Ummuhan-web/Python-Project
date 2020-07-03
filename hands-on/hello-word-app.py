61aa7b26b2598254c5f96f9e5637bd3884fad669
UMMUHAN <umm.yvz@gmail.com>
2020-07-03 01:14:06 +0000

    second commit

--- hands-on/hello-word-app.py
+++ hands-on/hello-word-app.py
@@ -4,8 +4,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello World'
+    return "ummuhan"+" yavuz"
+   
 
-if __name__=='_main': 
+if __name__=='__main__': 
    #app.run('localhost', port=5000, debug=True)
    app.run(debug=True)    
